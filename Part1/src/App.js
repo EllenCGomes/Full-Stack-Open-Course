f51a7371d45f32ed6b6e7a72f19fae34df5af2dc
Ellen Gomes <ellen_gomes14@hotmail.com>
2023-02-06 11:08:11 -0300

    Exercise 1.2: course information, step2

--- Part1/src/App.js
+++ Part1/src/App.js
@@ -6,15 +6,15 @@ const Header = (props) => (
 
 const Part = (props) => (
   <>
-    <p>{props.part}</p>
+    <p>{props.part} {props.exercise}</p>
   </>
 );
 
 const Content = (props) => (
   <>
-    <Part part={props.part1} />
-    <Part part={props.part2} />
-    <Part part={props.part3} />
+    <Part part={props.part1} exercise={props.exercises1} />
+    <Part part={props.part2} exercise={props.exercises2} />
+    <Part part={props.part3} exercise={props.exercises3} />
   </>
 );
 
@@ -36,7 +36,7 @@ const App = () => {
   return (
     <>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
       <Total total={exercises1 + exercises2 + exercises3} />
     </>
   )
