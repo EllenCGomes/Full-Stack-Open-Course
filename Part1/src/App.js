1ea6c3c2d0cb59a05c3e383ca61093ebde5e09ca
Ellen Gomes <ellen_gomes14@hotmail.com>
2023-02-07 09:17:03 -0300

    Exercise 1.3: course information, step 3

--- Part1/src/App.js
+++ Part1/src/App.js
@@ -1,22 +1,26 @@
-const Header = (props) => (
-  <>
-    <h1>{props.course}</h1>
-  </>
-);
+function Header({ course }) {
+  return (
+    <>
+      <h1>{course}</h1>
+    </>
+  )
+};
 
-const Part = (props) => (
-  <>
-    <p>{props.part} {props.exercise}</p>
-  </>
-);
+function Part({ part, exercise }) {
+  return (
+    <>
+      <p>{part} {exercise}</p>
+    </>
+  )
+};
 
-const Content = (props) => (
-  <>
-    <Part part={props.part1} exercise={props.exercises1} />
-    <Part part={props.part2} exercise={props.exercises2} />
-    <Part part={props.part3} exercise={props.exercises3} />
-  </>
-);
+function Content({ children }) {
+  return (
+    <>
+      {children}
+    </>
+  )
+};
 
 const Total = (props) => (
   <>
@@ -26,18 +30,28 @@ const Total = (props) => (
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  }
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  }
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  }
 
   return (
     <>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content>
+        <Part part={part1.name} exercise={part1.exercises} />
+        <Part part={part2.name} exercise={part2.exercises} />
+        <Part part={part3.name} exercise={part3.exercises} />
+      </Content>
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </>
   )
 };
