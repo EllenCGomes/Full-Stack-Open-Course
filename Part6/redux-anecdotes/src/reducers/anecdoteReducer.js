29febcb9121e603f409a6a519048866c2f161484
Ellen Gomes <ellen_gomes14@hotmail.com>
2023-05-03 13:00:45 -0300

    Exercises 6.14 - 6.15, steps 1 to 2 / Study Notes - notes-redux

--- Part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ Part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,38 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit"
-
-const anecdotesAtStart = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const anecdoteObject = (anecdote) => {
-    return {
-        content: anecdote,
-        id: getId(),
-        votes: 0
-    }
-}
-
-const initialState = anecdotesAtStart.map(anecdoteObject)
+import anecdoteService from "../services/anecdotes"
 
 const anecdoteSlice = createSlice({
     name: "anecdote",
-    initialState,
+    initialState: [],
     reducers: {
-        createAnecdote(state, action) {
-            const content = action.payload
-            state.push({
-                content,
-                id: getId(),
-                votes: 0
-            })
-        },
         vote(state, action) {
             const id = action.payload
             const anecdoteToVote = state.find(anecdote => anecdote.id === id)
@@ -42,8 +14,29 @@ const anecdoteSlice = createSlice({
             }
             return state.map(anecdote => anecdote.id !== id ? anecdote : anecdoteVoted)
         },
+        appendAnecdote(state, action) {
+            state.push(action.payload)
+        },
+        setAnecdotes(state, action) {
+            return action.payload
+        }
     }
 })
 
-export const { createAnecdote, vote } = anecdoteSlice.actions
+export const { vote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+    return async dispatch => {
+        const initialAnecdotes = await anecdoteService.getAll()
+        dispatch(setAnecdotes(initialAnecdotes))
+    }
+}
+
+export const createAnecdote = content => {
+    return async dispatch => {
+        const newAnecdote = await anecdoteService.createAnecdote(content)
+        dispatch(appendAnecdote(newAnecdote))
+    }
+}
+
 export default anecdoteSlice.reducer
--- StudyTests/notes-redux/src/App.js
+++ StudyTests/notes-redux/src/App.js
@@ -1,8 +1,17 @@
 import Notes from "./components/Notes";
 import NewNote from "./components/NewNote"
 import Filter from "./components/Filter";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { initializeNotes } from "./reducers/noteReducer";
 
 const App = () => {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(initializeNotes())
+  }, [dispatch])
 
   return (
     <div >
